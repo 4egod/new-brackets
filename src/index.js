dbf55022fd6f95d49a4146343e5a05c245ff05c9
Dmitry Tarasov <dmitry.tarasov@live.com>
2019-07-06 08:39:33 +0500

    fix for equal

--- src/index.js
+++ src/index.js
@@ -8,9 +8,22 @@ module.exports = function check(str, bracketsConfig) {
 
         for (var pair of bracketsConfig) {
             if (chr === pair[0]) {
-                stack.push(pair[1]);
-                pushed = true;
-                break;
+                if (pair[0] === pair[1]) {
+                    if (stack.length > 0 && stack[stack.length - 1] === chr) {
+                        stack.pop();
+                        pushed = true;
+                    }
+                    else {
+                        stack.push(pair[1]);
+                        pushed = true;
+                        break;
+                    }
+                }
+                else {
+                    stack.push(pair[1]);
+                    pushed = true;
+                    break;
+                }
             }
         }
 
