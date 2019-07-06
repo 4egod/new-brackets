64b0c6bec535256d5558b5efa5ac9c697ee562f9
Dmitry Tarasov <dmitry.tarasov@live.com>
2019-07-06 08:32:57 +0500

    rc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    var stack = [];
+
+    for (var chr of str) {
+
+        var pushed = false;
+
+        for (var pair of bracketsConfig) {
+            if (chr === pair[0]) {
+                stack.push(pair[1]);
+                pushed = true;
+                break;
+            }
+        }
+
+        if (!pushed && (stack.length === 0 || stack.pop() !== chr)) {
+            return false;
+        }
+    }
+
+    return stack.length === 0;
+};
